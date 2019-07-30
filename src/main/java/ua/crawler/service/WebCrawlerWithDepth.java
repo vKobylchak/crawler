53f3983447529c2ecc2ba1f8b8da4ec474dbb5f4
Vasyl Kobylchak <vkobylchak@corevalue.net>
2019-07-30 11:59:06 +0300

    change WebCrawler

--- src/main/java/ua/crawler/service/WebCrawlerWithDepth.java
+++ src/main/java/ua/crawler/service/WebCrawlerWithDepth.java
@@ -4,12 +4,16 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.HashSet;
 
+@Component
 public class WebCrawlerWithDepth {
-
+    @Value("${crawler.URL}")
+    private String url;
     private static final int MAX_DEPTH = 2;
     private HashSet<String> links;
 
@@ -24,12 +28,10 @@ public class WebCrawlerWithDepth {
     public void getPageLinks(String URL, int depth) {
         if ((!links.contains(URL) && (depth < MAX_DEPTH))) {
             try {
-                if (URL != "https://habr.com/en/all/")
+                if (URL != url)
                     links.add(URL);
-
                 Document document = Jsoup.connect(URL).get();
                 Elements linksOnPage = document.select(".post__title a[href]");
-
                 depth++;
                 for (Element page : linksOnPage) {
                     getPageLinks(page.attr("abs:href"), depth);
