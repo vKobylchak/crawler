9a3496d52af72fbf224fce2a45df447fc4c30087
Vasyl Kobylchak <vkobylchak@corevalue.net>
2019-07-30 11:17:32 +0300

    add CrawlerApplication

---
+++ src/main/java/ua/crawler/CrawlerApplication.java
@@ -0,0 +1,14 @@
+package ua.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerApplication.class, args);
+	}
+}
