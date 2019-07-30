989f0418b777c89200b116e91c4694b4e79dfcb9
Vasyl Kobylchak <vkobylchak@corevalue.net>
2019-07-30 11:45:55 +0300

    add @Value in ScheduledTasks

--- src/main/java/ua/crawler/service/ScheduledTasks.java
+++ src/main/java/ua/crawler/service/ScheduledTasks.java
@@ -7,6 +7,7 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ua.crawler.model.Link;
@@ -24,16 +25,25 @@ public class ScheduledTasks {
     @Autowired
     private TagService tagService;
 
+    @Value("${crawler.URL}")
+    private String URL;
+
+    @Value("${crawler.depth}")
+    private int depth;
+
+    @Value("${crawler.cssQuery}")
+    private String cssQuery;
+
     @Scheduled(fixedRate = 86400000)
     public void reportCurrentTime() {
         WebCrawlerWithDepth crawler = new WebCrawlerWithDepth();
-        crawler.getPageLinks("https://habr.com/en/all/", 0);
+        crawler.getPageLinks(URL, depth);
         for (String url : crawler.getLinks()) {
             Link link1 = new Link(url);
             linkService.addLink(link1);
             try {
                 Document document = Jsoup.connect(url).get();
-                Elements tags = document.select("a.inline-list__item-link.hub-link");
+                Elements tags = document.select(cssQuery);
                 for (Element tagElement : tags) {
                     Tag tag = new Tag(tagElement.text());
                     tag.getLinks().add(link1);
@@ -44,6 +54,6 @@ public class ScheduledTasks {
                 logger.warn("For '" + url + "': " + e.getMessage());
             }
         }
-        logger.info("success");
+        logger.info("create database");
     }
 }
