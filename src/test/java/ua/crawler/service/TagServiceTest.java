faaae24bea109d199453be8ed928f12877add384
Vasyl Kobylchak <vkobylchak@corevalue.net>
2019-08-05 16:04:27 +0300

    add TagServiceTest

---
+++ src/test/java/ua/crawler/service/TagServiceTest.java
@@ -0,0 +1,54 @@
+package ua.crawler.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import ua.crawler.entityHelper.TagHelper;
+import ua.crawler.model.Tag;
+import ua.crawler.repository.TagRepository;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TagServiceTest {
+
+    @Autowired
+    private TagService tagService;
+
+    @MockBean
+    private TagRepository tagRepository;
+
+    @Test
+    public void addTagTest() {
+        Tag tag = TagHelper.generateTag();
+
+        Mockito.when(tagRepository.save(tag)).thenReturn(tag);
+
+        assertThat(tagService.addTag(tag)).isEqualTo(tag);
+    }
+
+    @Test
+    public void findByNameTest() {
+        Tag tag = TagHelper.generateTag();
+
+        Mockito.when(tagRepository.findByName(tag.getName())).thenReturn(tag);
+
+        assertThat(tagService.findByName(tag.getName())).isEqualTo(tag);
+    }
+
+    @Test
+    public void findByIdTest() {
+        Tag tag = TagHelper.generateTag();
+
+        Mockito.when(tagRepository.findById(tag.getId())).thenReturn(Optional.of(tag));
+
+        assertThat(tagService.findById(tag.getId())).isEqualTo(tag);
+    }
+}
